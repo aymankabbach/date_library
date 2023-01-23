d907fbe9bf60a3da181ae7225851f5fa1f77dc90
ayman <kabbachayman95@gmail.com>
2023-01-23 17:26:00 +0100

    create the set & get functions for day/month/year

--- clsDate.h
+++ clsDate.h
@@ -4,5 +4,47 @@ using namespace std;
 
 class clsDate
 {
-
+private:
+    short _day=0;
+    short _month=0;
+    short _year=0;
+public:
+    void set_day(short day)
+    {
+        if (0<day && day<=31)
+        { 
+            _day=day;
+        }
+        else
+        {
+            cout<<day<<" is invalid"<<endl;
+        }
+    }
+    short get_day()
+    {
+        return _day;
+    }
+    void set_month(short month)
+    {
+        if (0<month && month<=12)
+        { 
+            _month=month;
+        }
+        else
+        {
+            cout<<month<<" is invalid"<<endl;
+        }
+    }
+    short get_month()
+    {
+        return _month;
+    }
+    void set_year(short year)
+    {
+        _year=year;
+    }
+    short get_year()
+    {
+        return _year;
+    }
 };
--- date_library.cpp
+++ date_library.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main()
 {
     clsDate Date;
+    Date.set_day(32);
+    Date.set_day(10);
+    Date.set_month(13);
+    Date.set_month(8);
+    Date.set_year(2020);
+    cout<<Date.get_day()<<"/"<<Date.get_month()<<"/"<<Date.get_year()<<endl;
     system("pause");
     return 0;
 }
